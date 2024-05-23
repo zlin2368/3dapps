391c82ed2cda136d3bc2d35a17820e9ab6671e63
zlin2368 <126585084+zlin2368@users.noreply.github.com>
2024-05-23 15:44:00 +0100

    Add files via upload

---
+++ assignment/index.php
@@ -0,0 +1,8 @@
+<?php
+// Enable strict error reporting for development purposes
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+
+// Include the MVC framework from the 'app' directory
+require 'app/mvc.php';
+?>
